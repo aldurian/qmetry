809dd4a98f63f7f4ed0cc3f4161a27402f9e1db0
alvin.l.durian <alvin.l.durian@MANU-6030QWL-LX.dir.svc.accenture.com>
2018-10-02 14:07:47 +0800

    Initial Setup

---
+++ src/qaf/example/tests/SampleTest.java
@@ -0,0 +1,20 @@
+package qaf.example.tests;
+
+import static com.qmetry.qaf.automation.step.CommonStep.get;
+import static com.qmetry.qaf.automation.step.CommonStep.verifyLinkWithPartialTextPresent;
+import static qaf.example.steps.StepsLibrary.searchFor;
+
+import org.openqa.selenium.support.ui.Wait;
+import org.testng.annotations.Test;
+
+import com.qmetry.qaf.automation.ui.WebDriverTestCase;
+
+public class SampleTest extends WebDriverTestCase {
+
+	@Test
+	public void testGoogleSearch() {
+		get("/");
+		searchFor("qaf github infostretch");
+		verifyLinkWithPartialTextPresent("qaf");
+	}
+}
